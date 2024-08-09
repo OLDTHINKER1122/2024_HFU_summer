303d4d65daf0f4ef7e4079182a685d77014b1fd8
OLDTHINKER1122 <a0908218158@gmail.com>
2024-08-09 15:36:05 +0800

    OPENAI_API_KEY PROTRCT ERROR

--- openai_sample.py
+++ openai_sample.py
@@ -3,8 +3,7 @@ from openai import OpenAI
 
 client = OpenAI(
     # This is the default and can be omitted
-    #api_key="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"
-    api_key=os.environ.get("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")
+    api_key=os.environ.get("OPENAI_API_KEY")
 )
 
 chat_completion = client.chat.completions.create(

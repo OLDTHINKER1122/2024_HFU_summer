9ce311244e468d5216722578f670ca1e7a3fe81e
OLDTHINKER1122 <a0908218158@gmail.com>
2024-08-09 10:45:01 +0800

    [refactor] flask app, handle secret key & tokens in other scripts

--- flask_linebot_wehook_server.py
+++ flask_linebot_wehook_server.py
@@ -21,23 +21,10 @@ from linebot.v3.webhooks import (
     MessageEvent, # 傳輸過來的方法
     TextMessageContent # 使用者傳過來的資料格式
 )
-import os, sys
+from handle_keys import get_secret_and_token
 
 app = Flask(__name__)
-
-# 1. 先到Line Developer Console，把 Channel Secret & Channel Access Token複製起來
-# 2. 把這兩個密文，存到環境變數內；工具列搜尋<環境變數>，新增兩個環境變數，並且把值貼上去
-# 3. 按下確定儲存，要記得你的變數名稱，這些資訊只會存在你當前使用的電腦裡。
-# 4. 透過以下程式碼，取得環境變數儲存的對應數值。
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
-
+keys = get_secret_and_token()
 handler = WebhookHandler(channel_secret)
 configuration = Configuration(access_token=channel_access_token)
 
@@ -46,12 +33,13 @@ configuration = Configuration(access_token=channel_access_token)
 def say_hello_world(username=""):
     return render_template("hello.html", name=username)
 
+
+@app.route("/callback", methods=['POST'])
+def callback():
 # 設計一個 #callback 的路由，提供給Line官方後台去呼叫
 # 也就所謂的呼叫Webhook Server
 # 因為官方會把使用者傳輸的訊息轉傳給Webhook Server
 # 所以會使用 RESTful API 的 POST 方法
-@app.route("/callback", methods=['POST'])
-def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
@@ -68,12 +56,13 @@ def callback():
 
     return 'OK'
 
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
 # 根據不同的使用者事件(event)，用不同的方式回應
 # eg. MessageEvent 代表使用者單純傳訊息的事件
 # TextMessageContent 代表使用者傳輸的訊息內容是文字
 # 符合兩個條件的事件，會被handle_message 所處理
-@handler.add(MessageEvent, message=TextMessageContent)
-def handle_message(event):
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
         line_bot_api.reply_message_with_http_info(

2e48535eae949106a31a7cda017b2742b2ad7194
OLDTHINKER1122 <a0908218158@gmail.com>
2024-08-15 17:07:11 +0800

    [feature] put text on image

--- image_model.py
+++ image_model.py
@@ -2,6 +2,7 @@
 import tensorflow as tf
 import cv2  # Install opencv-python
 import numpy as np
+import time
 
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
@@ -17,14 +18,13 @@ class_names = open("labels.txt", "r").readlines()
 camera = cv2.VideoCapture(0)
 
 while True:
+    start_time = time.time_ns()
+
     # Grab the webcamera's image.
-    ret, image = camera.read()
+    ret, raw_image = camera.read()
 
     # Resize the raw image into (224-height,224-width) pixels
-    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
-
-    # Show the image in a window
-    cv2.imshow("Webcam Image", image)
+    image = cv2.resize(raw_image, (224, 224), interpolation=cv2.INTER_AREA)
 
     # Make the image a numpy array and reshape it to the models input shape.
     image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
@@ -35,13 +35,31 @@ while True:
     # Predicts the model
     prediction = model(image)
     # prediction = model.predict(image)
-    index = np.argmax(prediction)
-    class_name = class_names[index]
-    confidence_score = prediction[0][index]
+    index = np.argmax(prediction) # 最高分的label的編號
+    class_name = class_names[index] # label編號的對應名稱
+    confidence_score = prediction[0][index] # label編號對應的分數
 
     # Print prediction and confidence score
-    print("Class:", class_name[2:], end="")
-    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+    top_one = class_name[2:].strip() # 預測的種類名稱
+    top_one_score = np.round(confidence_score * 100) # 預測的分數
+
+    # Show the image in a window, and put some text
+    if top_one_score >= 80:
+        text_to_put = f'{top_one}: {top_one_score}%'
+    else:
+        text_to_put = "Recognizing...."
+
+    cv2.putText(
+        img=raw_image,
+        text=str(text_to_put), 
+        org=(100, 100), 
+        fontFace=cv2.FONT_HERSHEY_SIMPLEX, 
+        fontScale=1,
+        color=(0, 255, 255), 
+        thickness=1, 
+        lineType=cv2.LINE_AA
+    )
+    cv2.imshow("Webcam Image", raw_image)
 
     # Listen to the keyboard for presses.
     keyboard_input = cv2.waitKey(1)
@@ -50,7 +68,9 @@ while True:
     if keyboard_input == 27:
         break
 
-    
+    period_time_in_ms = (time.time_ns() - start_time)/(10**6)
+    print("Check time: ", period_time_in_ms)
+
 
 camera.release()
 cv2.destroyAllWindows()
